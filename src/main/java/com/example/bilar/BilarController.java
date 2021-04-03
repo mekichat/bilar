a28e8a09d0b1156015cd15677726a620dc0e1ef1
mekichat <mekuria_chat@hotmail.com>
2021-04-03 03:37:22 +0200

    Added buildspec file

--- src/main/java/com/example/bilar/BilarController.java
+++ src/main/java/com/example/bilar/BilarController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Infrastructure As a Code
+
 @RestController
 public class BilarController {
 
